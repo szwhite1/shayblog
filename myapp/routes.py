742c3d708a1f9dc6eebfdf13f137f913a0ad4c7c
Shymeria White <shymeriawhite@yahoo.com>
2021-05-05 11:41:42 -0400

    Edited routes and index

--- myapp/routes.py
+++ myapp/routes.py
@@ -5,4 +5,14 @@ from myapp import app
 @app.route('/index')
 def index():
 	user = {'username': 'Shay'}
-	return render_template('index.html', title='Home', user=user)
+	posts = [
+		{
+			'author': {'username': 'John'},
+			'body': 'Beautiful day in Laurel!'
+		},
+		{
+			'author': {'username': 'Susan'},
+			'body': 'The Avengers movie is the best movie ever!'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
