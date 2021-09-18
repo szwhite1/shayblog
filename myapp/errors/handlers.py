96da8118866bc0186f18482fa707fba1b1c28c4d
Shymeria White <shymeriawhite@yahoo.com>
2021-09-18 16:37:55 -0400

    Fixed error handler

--- myapp/errors/handlers.py
+++ myapp/errors/handlers.py
@@ -4,9 +4,9 @@ from myapp.errors import bp
 
 @bp.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
 
 @bp.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('errors/500.html'), 500
