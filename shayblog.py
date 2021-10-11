4a26bca797a5ae32370182f5789f427d8e008ef8
Shymeria White <shymeriawhite@yahoo.com>
2021-10-11 13:53:08 -0400

    Added private messaging and user notifications

--- shayblog.py
+++ shayblog.py
@@ -1,5 +1,5 @@
 from myapp import create_app, db, cli
-from myapp.models import User, Post
+from myapp.models import User, Post, Notification, Message
 
 
 app = create_app()
@@ -8,4 +8,4 @@ cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User, 'Post': Post, 'Message': Message, 'Notification': Notification}
